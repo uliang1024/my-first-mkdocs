d94aa0f436163b3c77a6866068e72b6922a04211
2400215 <2400215@systex.con.tw>
2024-03-20 09:01:38 +0800

    加入.gitignore 與改變 .gitlab-ci.yml配置

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-let result = "";
-
-function onChange(event) {
-    let selectElement = event.target;
-    console.log(selectElement.value);
-    result = selectElement.value;
-    document.getElementById("result").innerText = result;
-}
